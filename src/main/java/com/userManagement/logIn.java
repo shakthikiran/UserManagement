9c7f9ca380b8e5dc7c867485688207e46451224d
shakthikiran <shakthikiran045@gmail.com>
2024-04-01 15:01:37 +0530

    Second Commit

--- src/main/java/com/userManagement/logIn.java
+++ src/main/java/com/userManagement/logIn.java
@@ -1,8 +1,5 @@
 package com.userManagement;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +10,7 @@ import java.io.IOException;
 
 
 public class logIn extends HttpServlet {
-	static final Logger logger = (Logger) LoggerFactory.getLogger(logIn.class);
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String uname=req.getParameter("uname");
@@ -31,7 +28,6 @@ public class logIn extends HttpServlet {
 		RequestDispatcher rd;
 		if(bean.validate()) {
 			rd = req.getRequestDispatcher("loginSuccess.jsp");
-			logger.info("Login Successful");
 		}
 		else {
 			rd = req.getRequestDispatcher("loginFailed.jsp");
